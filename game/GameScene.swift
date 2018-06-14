09215327ae6b2812f61f616417b0aa0948f05618
Marcus G <marcus.gardner@students.plymouth.ac.uk>
2018-06-14 21:24:11 +0100

    implemented player movement

--- game/GameScene.swift
+++ game/GameScene.swift
@@ -15,10 +15,12 @@ class GameScene: SKScene {
     
     let player = SKSpriteNode(imageNamed: "ship1.png")
     
+    
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.white
         
-        player.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        player.setScale(0.5)
+        player.position = CGPoint(x: self.size.width * 0.5, y: self.size.height * 0.1)
         
         addChild(player)
         
@@ -26,18 +28,21 @@ class GameScene: SKScene {
                                                           SKAction.wait(forDuration: 1.0)])
             ))
         
+//        run(SKAction.repeatForever(SKAction.sequence([SKAction.run(addMeteor),
+//        ))
+//
         
         
     }
     
-    func createContent(){
-        
-        let player = SKSpriteNode(imageNamed: "ship1.png")
-        
-        player.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        
-        self.addChild(player)
-    }
+//    func createContent(){
+//
+//        let player = SKSpriteNode(imageNamed: "ship1.png")
+//
+//        player.position = CGPoint(x: self.size.width * 0.5, y: self.size.height * 0.1)
+//
+//        self.addChild(player)
+//    }
     
     func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
@@ -49,8 +54,9 @@ class GameScene: SKScene {
     
     func addUFO(){
         //getting the ufo image
-        let ufo = SKSpriteNode(imageNamed: "ufoBlue.png")
+        let ufo = SKSpriteNode(imageNamed: "meteorGrey_med2.png")
         
+        //ufo.setScale(0.8)
         //random postion along the top of screen (x axis)
         let x = random(min: ufo.size.width/2, max: size.width - ufo.size.width/2)
         
@@ -63,12 +69,26 @@ class GameScene: SKScene {
         //ufo speed
         let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
         
+        
         //getting to ufo to move
         let actionMove = SKAction.move(to: CGPoint(x: x, y: -ufo.size.height/2), duration: TimeInterval(actualDuration))
         let actionMoveDone = SKAction.removeFromParent()
         ufo.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
     
+    func addMeteor(){
+        //adding the meteors
+        let meteor = SKSpriteNode(imageNamed: "")
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            player.position.x = location.x
+            player.position.y = location.y
+        }
+    }
   
     
 }
